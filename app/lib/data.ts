928af4a0198fb470b1333278de945e91eaecb721
Mauricio.Morocho <m.morocho1196@gmail.com>
2023-11-29 20:06:49 -0500

    Adding loading for component base

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,13 +19,13 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a reponse for demo purposes.
     // Don't do this in real life :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch complete after 3 seconds.');
+    // For testing purposes only, not prod
+    console.log('Data fetch complete after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -37,12 +37,19 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    // For testing purposes only, not prod
+    console.log('Fetching latest invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+    // For testing purposes only, not prod
+    console.log('Data fetch complete after 3 seconds.');
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
