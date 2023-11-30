0266350a50ae1704ab42ab26d101608b294c26c4
Mauricio.Morocho <m.morocho1196@gmail.com>
2023-11-29 21:37:52 -0500

    Grouping and wraping components to have loading

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -68,12 +68,17 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
+    console.log('Fetching latest invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
+
+    console.log('Data fetch complete after 3 seconds.');
 
     const data = await Promise.all([
       invoiceCountPromise,
