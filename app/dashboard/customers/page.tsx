4df0baf250784164ca5aa4a0ee547feec969f25e
Mauricio <m.morocho1196@gmail.com>
2025-05-28 22:48:28 -0500

    Adding customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,25 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Customers"
 }
 
-export default function Page() {
-  return <p>Customers page</p>
+interface PageProps {
+  searchParams?: {
+    query?: string;
+    page?: string
+  }
+}
+
+export default async function Page({ searchParams }: PageProps) {
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  )
 }
